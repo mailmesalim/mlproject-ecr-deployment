c37040b1b15d32ebdeccb941e791d62f2bef4ab5
mailmesalim <mailme.salim@gmail.com>
2026-01-17 20:42:58 +0000

    Flash & elsaticbeanstalk added

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,request,render_template,jsonify
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+application=Flask(__name__)
+app=application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form['gender'],
+            race_ethnicity=request.form['ethnicity'],
+            parental_level_of_education=request.form['parental_level_of_education'],
+            lunch=request.form['lunch'],
+            test_preparation_course=request.form['test_preparation_course'],
+            reading_score=request.form['reading_score'],
+            writing_score=request.form['writing_score']
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        predict_pipeline=PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        
+        # Check if it's an AJAX request
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+             return jsonify({'prediction': results[0]})
+        
+        return render_template('home.html',results=results[0])
+        
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
